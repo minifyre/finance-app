707fe87fdbe4067732f91e5ce8e238570ee8fa85
Minifyre <minifyre@gmail.com>
2020-02-01 15:17:56 -0600

    ability to view prior balances by date

--- src/index.js
+++ src/index.js
@@ -8,12 +8,21 @@ import {topLevelCategories} from 'enum'
 
 import data from './old/data.js'
 
-const mkState=()=>
-{
-	const transactions=utils.csv2json(data)
-		.filter(({from=''})=>!!from.length)
-		.map(({amount,...props})=>({...props,amount:amount.length?parseFloat(amount):0}))
+const defaultDate=new Date('1/1/2019')
 
+const mkTransactions=(csv=data)=>utils.csv2json(csv)
+	.filter(({from=''})=>!!from.length)
+	.map(({amount,...props})=>({...props,amount:amount.length?parseFloat(amount):0}))
+	// .map(
+	// 	({date,...props})=>(
+	// 		{date:(date.length?new Date(date):defaultDate).valueOf(),...props}
+	// 	)
+	// )
+
+
+
+const mkState=transactions=>
+{
 	const unorderedAccounts=transactions.reduce((accounts,{from,to,amount})=>
 	{
 		if(topLevelCategories.includes(from)) return Object.assign(accounts,{[to]:{parent:from,amount}})
@@ -42,16 +51,37 @@ const mkState=()=>
 	)
 	.reduce((totals,total,i)=>Object.assign(totals,{[topLevelCategories[i]]:total}),{})
 
-	return {accounts,transactions,subTotals}
+	return {accounts,subTotals}
 }
 
 const decimal2bgSize=decimal=>`background-size:${(decimal*100).toFixed(2)}% 100%;`
 
 export default ()=>
 {
-	const [state,setState]=useState(mkState())
+	//state
+	const [transactions]=useState(mkTransactions())
+	const [state,setState]=useState(mkState(transactions))
+	//view
+	const [dateString,setDate]=useState(new Date().toLocaleDateString('en-ca'))
+	console.log(state)
+	//ui
+	const header=h.header({},
+		h.input(
+			{
+				type:'date',
+				value:dateString,
+				onchange:evt=>
+				{
+					const dateString=evt.currentTarget.value
+					const viewDate=new Date(dateString)
 
-	return List({
+					setDate(dateString)
+					setState(mkState(transactions.filter(({date})=>new Date(date)<=viewDate)))
+				}
+			}
+		)
+	);
+	const list= List({
 		items:Object.entries(state.accounts),
 		ui:
 		{
@@ -69,4 +99,5 @@ export default ()=>
 			selectMultiple:false,
 		}
 	})
+	return [header,list]
 }
